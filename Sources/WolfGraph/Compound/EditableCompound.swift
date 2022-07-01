7fc4d5d668aef96e6e4e36d7167951c1e221496c
Wolf McNally <wolf@wolfmcnally.com>
2022-07-01 00:01:47 -0700

    Working on Compound.

--- Sources/WolfGraph/Compound/EditableCompound.swift
+++ Sources/WolfGraph/Compound/EditableCompound.swift
@@ -5,3 +5,19 @@ where InnerTree: EditableTree
 {
     func newNode(_ node: NodeID, data: NodeData, parent: NodeID, edge: EdgeID) throws -> Self
 }
+
+public extension EditableCompound
+where NodeData: DefaultConstructable
+{
+    func newNode(_ node: NodeID, parent: NodeID, edge: EdgeID) throws -> Self {
+        try newNode(node, data: NodeData(), parent: parent, edge: edge)
+    }
+}
+
+public extension EditableCompound
+where NodeData == Void
+{
+    func newNode(_ node: NodeID, parent: NodeID, edge: EdgeID) throws -> Self {
+        try newNode(node, data: (), parent: parent, edge: edge)
+    }
+}
--- Tests/WolfGraphTests/CompoundTests.swift
+++ Tests/WolfGraphTests/CompoundTests.swift
@@ -2,7 +2,7 @@ import XCTest
 import WolfGraph
 
 final class CompoundTests: XCTestCase {
-    func testCompound1() throws {
+    func testCompoundGraph() throws {
         typealias NodeID = String
         typealias EdgeID = String
         typealias NodeData = String
@@ -19,9 +19,35 @@ final class CompoundTests: XCTestCase {
         let compoundGraph = CompoundGraph()
         var compound = try MyCompound(graph: compoundGraph, tree: tree)
         compound = try compound
-            .newNode("A", data: "A", parent: "root", edge: "rA")
-            .newNode("B", data: "B", parent: "root", edge: "rB")
-            .newNode("C", data: "C", parent: "root", edge: "rC")
-        print(compound.jsonString)
+            .newNode("A", parent: "root", edge: "tree-rA")
+            .newNode("B", parent: "root", edge: "tree-rB")
+            .newNode("C", parent: "root", edge: "tree-rC")
+            .newNode("D", parent: "C", edge: "tree-CD")
+            .newEdge("AB", tail: "A", head: "B")
+        let json = #"{"graph":{"edges":{"AB":["A","B",""]},"nodes":{"A":"","B":"","C":"","D":""}},"tree":{"edges":{"tree-CD":["C","D"],"tree-rA":["root","A"],"tree-rB":["root","B"],"tree-rC":["root","C"]},"nodes":["A","B","C","D","root"],"root":"root"}}"#
+        XCTAssertEqual(compound.jsonString, json)
+    }
+    
+    func testCompoundDAG() throws {
+        typealias NodeID = TestGraph.NodeID
+        typealias EdgeID = TestGraph.EdgeID
+        typealias NodeData = TestGraph.NodeData
+        typealias EdgeData = TestGraph.EdgeData
+
+        typealias TreeGraph = Graph<NodeID, EdgeID, Empty, Empty>
+        typealias CompoundTree = Tree<TreeGraph>
+        typealias CompoundDAG = DAG<TestGraph>
+        typealias MyCompound = Compound<CompoundDAG, CompoundTree>
+        
+        let root = "root"
+        let treeGraph = try TreeGraph().newNode("root")
+        var tree = try CompoundTree(graph: treeGraph, root: root)
+        let compoundDAG = try CompoundDAG(graph: TestGraph.makeDAG())
+        for node in compoundDAG.nodes {
+            tree = try tree.newNode(node, parent: "root", edge: "r\(node)")
+        }
+        let compound = try MyCompound(graph: compoundDAG, tree: tree)
+        let json = #"{"graph":{"edges":{"AC":["A","C","AC"],"AD":["A","D","AD"],"AE":["A","E","AE"],"BA":["B","A","BA"],"BC":["B","C","BC"],"BG":["B","G","BG"],"CD":["C","D","CD"],"ED":["E","D","ED"],"FD":["F","D","FD"],"FE":["F","E","FE"],"GI":["I","G","GI"],"HJ":["H","J","HJ"],"IB":["B","I","IB"],"IC":["I","C","IC"],"IK":["I","K","IK"],"JA":["J","A","JA"],"JE":["J","E","JE"],"JF":["J","F","JF"]},"nodes":{"A":"A","B":"B","C":"C","D":"D","E":"E","F":"F","G":"G","H":"H","I":"I","J":"J","K":"K"}},"tree":{"edges":{"rA":["root","A"],"rB":["root","B"],"rC":["root","C"],"rD":["root","D"],"rE":["root","E"],"rF":["root","F"],"rG":["root","G"],"rH":["root","H"],"rI":["root","I"],"rJ":["root","J"],"rK":["root","K"]},"nodes":["A","B","C","D","E","F","G","H","I","J","K","root"],"root":"root"}}"#
+        XCTAssertEqual(compound.jsonString, json)
     }
 }
