502aff3a664559aecfd717c7e4525e54ef0a647f
Wolf McNally <wolf@wolfmcnally.com>
2022-07-03 14:47:49 -0700

    Cleanup.

--- Tests/GraphTests/TestGraph.swift
+++ Tests/GraphTests/TestGraph.swift
@@ -49,29 +49,6 @@ extension TestGraph {
 }
 
 extension TestGraph {
-    static func makeDAG() -> Self {
-        try! Self(edges: [
-            ("AC", "A", "C"),
-            ("AD", "A", "D"),
-            ("AE", "A", "E"),
-            ("BA", "B", "A"),
-            ("BC", "B", "C"),
-            ("BG", "B", "G"),
-            ("CD", "C", "D"),
-            ("ED", "E", "D"),
-            ("FD", "F", "D"),
-            ("FE", "F", "E"),
-            ("HJ", "H", "J"),
-            ("IC", "I", "C"),
-            ("IK", "I", "K"),
-            ("JA", "J", "A"),
-            ("JE", "J", "E"),
-            ("JF", "J", "F"),
-            ("GI", "I", "G"),
-            ("IB", "B", "I"),
-        ])
-    }
-    
     static func makeGraph() -> Self {
         try! Self(edges: [
             ("AC", "A", "C"),
@@ -95,6 +72,99 @@ extension TestGraph {
         ])
     }
     
+    // http://magjac.com/graphviz-visual-editor/
+    /*
+     digraph G {
+         A [label="A"]
+         B [label="B"]
+         C [label="C"]
+         D [label="D"]
+         E [label="E"]
+         F [label="F"]
+         G [label="G"]
+         H [label="H"]
+         I [label="I"]
+         J [label="J"]
+         K [label="K"]
+         A -> C [label="AC"]
+         A -> D [label="AD"]
+         A -> E [label="AE"]
+         B -> A [label="BA"]
+         B -> C [label="BC"]
+         B -> G [label="BG"]
+         C -> D [label="CD"]
+         E -> D [label="ED"]
+         F -> D [label="FD"]
+         F -> E [label="FE"]
+         G -> I [label="GI"]
+         H -> J [label="HJ"]
+         I -> B [label="IB"]
+         I -> C [label="IC"]
+         I -> K [label="IK"]
+         J -> A [label="JA"]
+         J -> E [label="JE"]
+         J -> F [label="JF"]
+     }
+     */
+    
+    static func makeDAG() -> Self {
+        try! Self(edges: [
+            ("AC", "A", "C"),
+            ("AD", "A", "D"),
+            ("AE", "A", "E"),
+            ("BA", "B", "A"),
+            ("BC", "B", "C"),
+            ("BG", "B", "G"),
+            ("CD", "C", "D"),
+            ("ED", "E", "D"),
+            ("FD", "F", "D"),
+            ("FE", "F", "E"),
+            ("HJ", "H", "J"),
+            ("IC", "I", "C"),
+            ("IK", "I", "K"),
+            ("JA", "J", "A"),
+            ("JE", "J", "E"),
+            ("JF", "J", "F"),
+            ("GI", "I", "G"),
+            ("IB", "B", "I"),
+        ])
+    }
+    
+    // http://magjac.com/graphviz-visual-editor/
+    /*
+     digraph G {
+         A [label="A"]
+         B [label="B"]
+         C [label="C"]
+         D [label="D"]
+         E [label="E"]
+         F [label="F"]
+         G [label="G"]
+         H [label="H"]
+         I [label="I"]
+         J [label="J"]
+         K [label="K"]
+         A -> C [label="AC"]
+         A -> D [label="AD"]
+         A -> E [label="AE"]
+         B -> A [label="BA"]
+         B -> C [label="BC"]
+         B -> G [label="BG"]
+         C -> D [label="CD"]
+         E -> D [label="ED"]
+         F -> D [label="FD"]
+         F -> E [label="FE"]
+         I -> G [label="GI"]
+         H -> J [label="HJ"]
+         B -> I [label="IB"]
+         I -> C [label="IC"]
+         I -> K [label="IK"]
+         J -> A [label="JA"]
+         J -> E [label="JE"]
+         J -> F [label="JF"]
+     }
+     */
+    
     static func makeTree() -> Self {
         try! Self(edges: [
             ("AB", "A", "B"),
@@ -113,4 +183,39 @@ extension TestGraph {
             ("EO", "E", "O"),
         ])
     }
+
+    // http://magjac.com/graphviz-visual-editor/
+    /*
+     digraph G {
+         A [label="A"]
+         B [label="B"]
+         C [label="C"]
+         D [label="D"]
+         E [label="E"]
+         F [label="F"]
+         G [label="G"]
+         H [label="H"]
+         I [label="I"]
+         J [label="J"]
+         K [label="K"]
+         L [label="L"]
+         M [label="M"]
+         N [label="N"]
+         O [label="O"]
+         A -> B [label="AB"]
+         A -> C [label="AC"]
+         A -> D [label="AD"]
+         B -> I [label="BI"]
+         C -> H [label="CH"]
+         D -> E [label="DE"]
+         D -> F [label="DF"]
+         D -> G [label="DG"]
+         E -> M [label="EM"]
+         E -> N [label="EN"]
+         E -> O [label="EO"]
+         F -> L [label="FL"]
+         H -> J [label="HJ"]
+         H -> K [label="HK"]
+     }
+     */
 }
