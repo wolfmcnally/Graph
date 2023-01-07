04538aebf46f120dcce1392a215a7a850c45b4e6
Wolf McNally <wolf@wolfmcnally.com>
2023-01-07 15:11:06 -0800

    Removed platform requirements.

--- Package.swift
+++ Package.swift
@@ -4,16 +4,15 @@ import PackageDescription
 
 let package = Package(
     name: "Graph",
-    platforms: [.macOS(.v13), .iOS(.v15)],
     products: [
         .library(
             name: "Graph",
             targets: ["Graph"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/wolfmcnally/WolfBase.git", .upToNextMajor(from: "4.0.0")),
-        .package(url: "https://github.com/wolfmcnally/FUID.git", .upToNextMajor(from: "0.1.0")),
-        .package(url: "https://github.com/apple/swift-collections", branch: "main"),
+        .package(url: "https://github.com/wolfmcnally/WolfBase.git", .upToNextMajor(from: "5.0.0")),
+        .package(url: "https://github.com/wolfmcnally/FUID.git", .upToNextMajor(from: "0.3.0")),
+        .package(url: "https://github.com/wolfmcnally/swift-collections", .upToNextMajor(from: "1.1.0")),
         .package(url: "https://github.com/apple/swift-algorithms", .upToNextMajor(from: "1.0.0")),
     ],
     targets: [
